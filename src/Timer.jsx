c8b727feab28be48f756ee577150baaa89cf0704
lezyCoder <iamvijaysingh23@gmail.com>
2025-12-12 02:42:02 +0530

    Component Fixed

---
+++ src/Timer.jsx
@@ -0,0 +1,10 @@
+
+const Timer = () => {
+    return (
+        <div className="w-1/2  flex justify-center items-center px-4 pt-10">
+            Timer container
+        </div>
+    )
+}
+
+export default Timer
