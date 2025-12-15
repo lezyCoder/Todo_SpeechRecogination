cac09ab166cd10f2b46b18271d3bce78c0d46ce9
lezyCoder <iamvijaysingh23@gmail.com>
2025-12-16 04:59:16 +0530

    Working on Marked Completed features

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -66,11 +66,8 @@ const Todo = () => {
         setEditingIndex(indexToEdit);
     }
 
-    // Handling the updated task
-    const handleCompletedTask = (e) => {
-        const todoItem = e.target.closest(".todo-item");
-        if (!todoItem) return;
-        todoItem.classList.toggle("completed", e.target.checked);
+    const handleCompletedTask = (indexMarked) => {
+      
     }
     //  Speech handling (Speech to text )
     const handleSpeech = () => {
@@ -98,81 +95,86 @@ const Todo = () => {
     };
 
     return (
-        <div className="todo-Container w-1/2 flex flex-col px-4 justify-center overflow-auto" >
-            <form onSubmit={handleSubmit} className="w-full p-2 flex gap-4 relative">
+        <div className="todo-Container w-1/2  px-4" >
+            <div className="todo flex flex-col h-full gap-2">
+                <div className="form-container">
+                    <form onSubmit={handleSubmit} className=" w-full p-2 flex gap-4">
 
-                <div className="relative w-2/3">
-                    <input
-                        type="text"
-                        placeholder="Enter task"
-                        value={todo}
-                        onChange={(e) => setTodo(e.target.value)}
-                        className="border border-white rounded text-white p-2 w-full pr-12 outline-none"
-                    />
+                        <div className="relative w-2/3">
+                            <input
+                                type="text"
+                                placeholder="Enter task"
+                                value={todo}
+                                onChange={(e) => setTodo(e.target.value)}
+                                className="border border-white rounded text-white p-2 w-full pr-12 outline-none"
+                            />
 
-                    {/* Mic button inside input */}
-                    <button
-                        type="button"
-                        onClick={handleSpeech}
-                        className="mic-button absolute right-0 top-0 h-full px-3 flex items-center justify-center 
+                            {/* Mic button inside input */}
+                            <button
+                                type="button"
+                                onClick={handleSpeech}
+                                className="mic-button absolute right-0 top-0 h-full px-3 flex items-center justify-center 
                  border-l border-white bg-transparent cursor-pointer"
-                    >
-                        <FaMicrophone className="text-zinc-100 opacity-80" />
-                    </button>
-                </div>
-
-                <div className="btns flex gap-3">
-                    <button
-                        type="submit"
-                        className="outline-none cursor-pointer rounded p-2 w-20 ml-4 bg-green-800 
-                 hover:scale-[0.9] ease-in-out"
-                    >
-                        {editingIndex !== null ? "Update" : "Add Task"}
-                    </button>
-
-                    <button
-                        type="button"
-                        className="outline-none rounded bg-red-800 p-2 w-20 hover:scale-[0.9] ease-in-out cursor-pointer"
-                        onClick={() => {
-                            setTodo("");
-                            setEditingIndex(null);
-                        }}
-                    >
-                        Cancel
-                    </button>
-                </div>
-            </form>
-
-            <ul className='w-full  p-2 '  >
-                {
-                    todos.map((task, index) => (
-                        <div
-                            className={`todo-item flex justify-between items-center p-4 mb-2 ${editingIndex === index ? 'bg-blue-900' : 'bg-gray-800'
-                                }`}
-                            key={index}
-                        >
-                            <li className='text-white'>{task}</li>
-                            <div className='btns gap-2 flex'>
-                                <input type="checkbox" className='w-10 outline-none' onClick={() => handleCompletedTask(e)} />
-                                <button
-                                    type="button"
-                                    className='border border-green-300 p-2 rounded'
-                                    onClick={() => handleEdit(index)}
-                                >
-                                    Edit
-                                </button>
-                                <button
-                                    type="button"
-                                    className='border border-red-300 p-2 rounded'
-                                    onClick={() => handleDelete(index)}
-                                >
-                                    Delete
-                                </button>
-                            </div>
+                            >
+                                <FaMicrophone className="text-zinc-100 opacity-80" />
+                            </button>
                         </div>
-                    ))
-                }
-            </ul>
+
+                        <div className="btns flex gap-3">
+                            <button
+                                type="submit"
+                                className="outline-none cursor-pointer rounded p-2 w-20 ml-4 bg-green-800 
+                 hover:scale-[0.9] ease-in-out"
+                            >
+                                {editingIndex !== null ? "Update" : "Add Task"}
+                            </button>
+
+                            <button
+                                type="button"
+                                className="outline-none rounded bg-red-800 p-2 w-20 hover:scale-[0.9] ease-in-out cursor-pointer"
+                                onClick={() => {
+                                    setTodo("");
+                                    setEditingIndex(null);
+                                }}
+                            >
+                                Cancel
+                            </button>
+                        </div>
+                    </form>
+                </div>
+                <div className="todo-items">
+                    <ul className='w-full p-2'>
+                        {
+                            todos.map((task, index) => (
+                                <div
+                                    className={`todo-item flex justify-between items-center p-4 mb-2${task.completed ? "completed" : ""}  ${editingIndex === index ? 'bg-blue-900' : 'bg-gray-800'
+                                        }`}
+                                    key={index}
+                                >
+                                    <li className='text-white'>{task}</li>
+                                    <div className='btns gap-2 flex'>
+                                        <input type="checkbox" className='w-10 outline-none' onChange={() => handleCompletedTask(index)} />
+                                        <button
+                                            type="button"
+                                            className='border border-green-300 p-2 rounded'
+                                            onClick={() => handleEdit(index)}
+                                        >
+                                            Edit
+                                        </button>
+                                        <button
+                                            type="button"
+                                            className='border border-red-300 p-2 rounded'
+                                            onClick={() => handleDelete(index)}
+                                        >
+                                            Delete
+                                        </button>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
