4b428623af9c223f5de834153e138ec0f5e8245b
lezyCoder <iamvijaysingh23@gmail.com>
2025-12-20 00:01:36 +0530

    height style updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from './Navbar'
 
 const App = () => {
   return (
-    <div className='bg-zinc-700 w-full h-dvh text-white'>
+    <div className='bg-zinc-700 w-full min-h-dvh text-white'>
       <Todos />
     </div>
   )
